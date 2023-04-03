8d349b3320cf29bffac82373bb6eae854dd501c1
Rahat <99819433+rahat49@users.noreply.github.com>
2023-04-03 22:57:53 +0600

    Added project files to repository

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Calculator;
+
+public static class Calculator
+	{
+		public static double Calculate(double val1, double val2, string mathOperator)
+		{
+
+			double result = 0;
+			switch (mathOperator)
+			{
+				case "÷":
+					result = val1 / val2;
+					break;
+                case "×":
+                    result = val1 * val2;
+                    break;
+                case "+":
+                    result = val1 + val2;
+                    break;
+                case "−":
+                    result = val1 - val2;
+                    break;
+
+            }
+			return result;
+		}
+		
+	}
+public static class DoubleExtensions
+{
+	public static string ToTrimmedString (this double target, string decimalFormat)
+	{
+		string strValue = target.ToString(decimalFormat);
+
+		if (strValue.Contains(".")) 
+		{
+			strValue = strValue.TrimEnd('0');
+
+			if (strValue.EndsWith("."))
+				strValue = strValue.TrimEnd('.');
+
+		}
+		return strValue;
+	}
+}
+
+
---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		OnClear(this, null);
+	}
+	string currentEntry = "";
+	int currentState = 1;
+	string mathOperator;
+	double firstNum, secondNum;
+	string decimalFormat = "N0";
+
+	void OnClear(object sender, EventArgs e)
+	{
+		firstNum = 0;
+		secondNum = 0;
+		currentState = 1;
+		decimalFormat = "N0";
+		this.resultText.Text = "0";
+		currentEntry = string.Empty;
+	}
+
+	void OnSelectNumber(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+		string pressed = button.Text;
+
+		currentEntry += pressed;
+
+		if((this.resultText.Text == "0" && pressed == "0")
+			||(currentEntry.Length<=1 && pressed != "0")|| currentState<0)
+		{
+			this.resultText.Text = "";
+			if (currentState < 0)
+				currentState *= -1;
+
+		}
+		if(pressed == "." && decimalFormat !="N2")
+		{
+			decimalFormat = "N2";
+		}
+		this.resultText.Text += pressed;
+	}
+
+	void OnSelectOperator (object sender, EventArgs e)
+	{
+		LockNumberValue(resultText.Text);
+		currentState = -2;
+		Button button = (Button)sender;
+		string pressed = button.Text;
+		mathOperator = pressed;
+
+
+	}
+
+    private void LockNumberValue(string text)
+    {
+		double number;
+		if(double.TryParse(text, out number))
+		{
+			if(currentState==1)
+			{
+				firstNum = number;
+
+			}
+			else
+			{
+				secondNum = number;
+			}
+			currentEntry = string.Empty;
+		}
+    }
+
+	void OnCalculate (object sender, EventArgs e)
+	{
+		if(currentState==2)
+		{
+			if (secondNum == 0)
+                decimalFormat = "N2";
+            LockNumberValue(resultText.Text);
+			double result = Calculator.Calculate(firstNum, secondNum, mathOperator);
+
+			this.CurrentCalculation.Text = $"{firstNum} {mathOperator} {secondNum}";
+			this.resultText.Text = result.ToTrimmedString(decimalFormat);
+			firstNum = result;
+			secondNum = 0;
+			currentState = -1;
+			currentEntry = string.Empty;
+		}
+	}
+    void OnNegative(object sender, EventArgs e)
+    {
+        if (resultText.Text.StartsWith("-"))
+        {
+			resultText.Text = resultText.Text.Substring(1);
+        }
+		else if(!string.IsNullOrEmpty(resultText.Text)&&decimal.Parse(resultText.Text) !=0)
+		{
+			resultText.Text = "-" + resultText.Text;
+		}
+    }
+
+    void OnPercentage(object sender, EventArgs e)
+    {
+        if (currentState == 1)
+        {
+            LockNumberValue(resultText.Text);
+            decimalFormat = "N2";
+			firstNum = 0.01;
+            mathOperator = "×";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+
+}
+
+
